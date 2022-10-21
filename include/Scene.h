c605ea02d79660bb7f28bbe81a5a8d5b90edd539
gabrielLiendo <gabo.c.liendo@gmail.com>
2022-10-20 22:58:36 -0400

    select Object method changed

--- include/Scene.h
+++ include/Scene.h
@@ -71,12 +71,15 @@ public:
 	{
 		if (selectedObject)
 		{
-			/*
-			std::vector<Object>::size_type i = selectedObject - &objects[0];
-			assert(i < objects.size());
-			std::vector<Object>::iterator it = objects.begin() + i;
-			objects.erase(it);
-			selectedObject = nullptr;*/
+			for(auto it = objects.begin(); it != objects.end(); ++it)
+			{
+				if ((*it) == selectedObject)
+				{
+					objects.erase(it);
+					selectedObject = nullptr;
+					return;
+				}
+			}
 		}
 	}
 
